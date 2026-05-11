dc733d8a443afec90967aaa62e1670448feb358b
akhilprakash634 <akhilprakash634@gmail.com>
2026-05-11 10:34:30 +0530

    feat: implement WhatsApp button and update landing page assets and configuration

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { AppRoutes } from "./router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import SocialPopup from "./components/SocialPopup";
-
+import WhatsAppButton from "./components/WhatsAppButton";
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
       <BrowserRouter basename={__BASE_PATH__}>
         <AppRoutes />
         <SocialPopup />
+        <WhatsAppButton />
       </BrowserRouter>
     </I18nextProvider>
   );
